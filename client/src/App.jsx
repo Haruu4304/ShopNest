5465b98ce2f6bab51d5514d2fa4011eb09966e35
Harsha Surwase <ss21co056harsha@gmail.com>
2025-06-28 14:28:36 +0530

    Updated Readme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import  { Toaster } from "react-hot-toast";
-
+//
 const App = () => {
   return (
     <>
